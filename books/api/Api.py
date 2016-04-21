db9a22012408c5f21673a41e48ad172a0150dcc2
Auggie Quijano <auggie.quijano@voyagerinnovation.com>
2016-04-21 16:38:05 +0800

    corrected indentation

--- books/api/Api.py
+++ books/api/Api.py
@@ -1,8 +1,12 @@
 #$Id$#
 
 class Api:
-    """This class is used to maintain the service URL."""
+     """This class is used to maintain the service URL."""
+     
+     def __init__(self):
+         """Initialize parameters for API class."""
+ 
+         self.base_url = 'https://books.zoho.com/api/v3/'
 
-    def __init__(self):
-        """this is my comment"""
-        self.base_url = 'https://books.zoho.com/api/v3/'
+    
+     
